60d1fa5c5e169e8022aabae428d81c925a1d09de
pyash7580 <134642381+pyash7580@users.noreply.github.com>
2024-04-25 01:38:48 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,7 @@
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener('input',() => {
+    sliderValue.textContent = inputSlider.value;    
+}); 
